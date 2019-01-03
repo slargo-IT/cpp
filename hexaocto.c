a25cf5c413461d476619bbf8ba7c1083e86ae67b
slargo-IT <42444726+slargo-IT@users.noreply.github.com>
2019-01-03 16:40:18 +0100

    Create hexaocto.c
    yeh

---
+++ hexaocto.c
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/*
+ en tete incluses : ios, iosfwd, istream, ostream, streamduf
+*/
+
+//ios base initialise classe de bases
+// décimale, octale, hexa
+
+using namespace std;
+
+int main()
+{
+    std::cout << std::oct;
+    std::cout << 146 << std::endl;
+    std::cout << std::dec << 1544 << std::endl;
+
+    return 0;
+}
